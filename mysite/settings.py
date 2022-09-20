dabeadfc61969eb65c01798e3d5cb349091d4090
gobinath shanthakumar <gobinathshanthakumar@gobinaths-MacBook-Air.local>
2022-09-20 08:36:45 -0400

    new fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -157,3 +157,5 @@ CHANNEL_LAYERS = {
         },
     },
 }
+
+django_heroku.settings(locals())
